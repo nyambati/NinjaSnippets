19818b60d7bd6c8b1298f1bf49a0ebdb13ec2a89
Thomas Nyambati <thomas.nyambati@andela.com>
2016-09-13 19:49:26 +0100

    Modify install to remove installation

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+set -ex
 
 # Sublime and vscode folders
 VSCODE_SNIPPET_FOLDER="vscode"
 SUBLIME_SNIPPET_FOLDER="ninjaSnippet"
+PROGRAMME=$0
 
+# check which machine is running the scipt
 if [[ uname -eq "Darwin" ]]; then
     SUBLIME_PACKAGE=$HOME/Library/Application\ Support/sublime\ Text\ 3/Packages
     VSCODE_PACKAGE=$HOME/Library/Application\ Support/Code/User/snippets
@@ -12,19 +15,21 @@ if [[ uname -eq "Darwin" ]]; then
     exit 0
 fi
 
-#
+# script  for installing sublime snippet
 function install_sublime_snippet() {
   if [[ -d "${SUBLIME_SNIPPET_FOLDER}" ]]; then
-    cp -R $SUBLIME_SNIPPET_FOLDER "${SUBLIME_PACKAGE}/${SUBLIME_SNIPPET_FOLDER}"
+    cp -av $SUBLIME_SNIPPET_FOLDER/ "${SUBLIME_PACKAGE}/${SUBLIME_SNIPPET_FOLDER}"
   fi
 }
 
+# Insralling vscode snippets
 function install_vscode_snippet() {
   if [[ -d "${VSCODE_SNIPPET_FOLDER}" ]]; then
     cp -R $VSCODE_SNIPPET_FOLDER/* "${VSCODE_PACKAGE}"
   fi
 }
 
+# User feedback for installing vscode
 function install_vscode() {
 
   install_vscode_snippet
@@ -42,6 +47,7 @@ function install_vscode() {
   fi
 }
 
+# User feedfack for installing sublime snippets
 function install_sublime() {
 
   install_sublime_snippet
@@ -59,19 +65,34 @@ function install_sublime() {
   fi
 }
 
-while getopts ":i:r:h:" opt; do
+# program usage function
+function programme_usage {
+    echo "usage: $PROGRAMME [-ibch] <editor>"
+    echo "  -i    Install snippets"
+    echo "  -r    uninstall snippets"
+    echo "  -h    display help"
+    exit 1
+}
+
+if [[ $# -eq 0 ]]; then
+  programme_usage
+fi
+
+# usage prompr por ninja script
+while getopts ":i:r:l:h" opt; do
   case $opt in
     i)
       if [[ "${OPTARG}" == "sublime" ]]; then
         install_sublime
       elif [[ "${OPTARG}" == "vscode" ]]; then
         install_vscode
+      else
+        echo "Commad ${OPTARG} was not found"
       fi
       ;;
+    r) EDITOR=$OPTARG;;
+    l) EDITOR_LANG=$OPTARG;;
     h)
-      echo "-a was triggered, Parameter: $OPTARG" >&2
-      ;;
-    r)
       echo "-a was triggered, Parameter: $OPTARG" >&2
       ;;
     \?)
@@ -85,17 +106,38 @@ while getopts ":i:r:h:" opt; do
   esac
 done
 
-PROGRAMME=$0
-
-function programme_usage {
-    echo "usage: $PROGRAMME [-ibch] <editor>"
-    echo "  -i    Install snippets"
-    echo "  -r    uninstall snippets"
-    echo "  -h    display help"
+# uninstall snippet
+echo " this is the editot $EDITOR_LANG"
+function uninstall_vscode() {
+  rm -f "${VSCODE_PACKAGE}/${EDITOR_LANG}.json"
+  if [[ "$?" -eq "0" ]]; then
+    echo "============================================================="
+    echo "Snippet for language ${EDITOR_LANG} removed successfully"
+    echo " "
+    echo " "
+    exit 0
+  else
+    echo "============================================================="
+    echo "snipptet for language ${EDITOR_LANG} failed, please try again"
+    echo " "
+    echo " "
     exit 1
+  fi
 }
 
-if [ $# -eq 0 ]
-  then
-    programme_usage
+function uninstall_sublime() {
+  echo "============================================"
+  echo "   unistalling sublime ninjaSnippet"
+  rm -rf "${SUBLIME_PACKAGE}/${SUBLIME_SNIPPET_FOLDER}"
+}
+
+shift $(( OPTIND - 1 ))
+
+if [ "$EDITOR" == "vscode" ] && [ -n "${EDITOR_LANG}" ]; then
+  uninstall_vscode
+elif [[ "${EDITOR}" == 'sublime' ]]; then
+  uninstall_sublime
+else
+  echo "Missing or incorrent argument, check and try again"
 fi
+
